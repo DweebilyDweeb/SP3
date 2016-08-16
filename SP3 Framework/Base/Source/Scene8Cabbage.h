8381dd8c0cc996774b35ab6a1052617aa2711f2d
Hidayat <hidhandsome143@gmail.com>
2016-08-17 00:52:14 +0800

    Added the Scenes for the 3 scenes I am doing (Chicken, Wheat and Cabbage) with their csv and QW Scene2Cow csv

---
+++ SP3 Framework/Base/Source/Scene8Cabbage.h
@@ -0,0 +1,74 @@
+#ifndef SCENE_8_CABBAGE_H
+#define SCENE_8_CABBAGE_H
+
+#include "Scene3D.h"
+#include "Mesh.h"
+#include "SpriteAnimation.h"
+#include "TileMap.h"
+#include "Camera2D.h"
+#include "PlayerSS.h"
+
+class Scene8Cabbage : public Scene3D {
+
+private:
+	enum GEOMETRY_TYPE {
+		//Tiles
+		GEO_EMPTY,
+		GEO_TILE_BRICK,
+		GEO_SPIKE,
+		GEO_BOUNCE,
+		GEO_PORTAL,
+		GEO_FLAG,
+		GEO_POLE,
+		GEO_BLOCK,
+		GEO_BACKGROUND_1,
+		GEO_BACKGROUND_2,
+		GEO_BACKGROUND_3,
+
+		//Others
+		GEO_PLAYER,
+
+		NUM_GEOMETRY,
+	};
+
+	enum SPRITE_TYPE {
+		SPRITE_PENGUIN,
+
+		NUM_SPRITE,
+	};
+
+	Mesh* meshList[NUM_GEOMETRY];
+	SpriteAnimation* spriteAnimationList[NUM_SPRITE];
+
+	TileMap tileMap;
+
+	void InitMeshes();
+	void InitSpriteAnimations();
+	void InitCamera();
+	void InitPlayer();
+
+	void RenderTileMap();
+	void RenderPlayer();
+	void RenderBackground();
+	void RenderText();
+
+	Camera2D camera;
+	PlayerSS player;
+
+	float drop;
+	int Level;
+
+public:
+	//Constructor(s) & Destructor
+	Scene8Cabbage();
+	virtual ~Scene8Cabbage();
+
+	//Virtual Function(s)
+	virtual void Init();
+	virtual void Update(const double& deltaTime);
+	virtual void Render();
+	virtual void Exit();
+
+};
+
+#endif
